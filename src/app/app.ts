4249445272f0e0f520a0ecc955d509f330e1d58d
CarlosQE <quirogaespanajosecarlos@gmail.com>
2026-04-28 00:55:39 -0400

    feat: proyecto Angular inicial - lecciones 1-4 data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('condoadmin-web');
-}
+  styleUrls: ['./app.css'],
+  imports: [FormsModule]})
+
+  export class App {
+    titulo = 'CondoAdmin';
+    subtitulo = 'Sistema de adminsitracion de condominios';
+    totalEdificios = 2;
+    totalUnidades = 6;
+
+    menuAbierto = false;
+
+    busqueda = ''
+
+    toggleMenu() {
+      this.menuAbierto = !this.menuAbierto;
+    }
+  }
+  
